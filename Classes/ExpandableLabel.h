704c54b0ea66b189e8b9dfa02f512f8805a082a1
feixue299 <ariablink299@gmail.com>
2020-06-10 18:29:26 +0800

    Update ExpandableLabel.h

--- Classes/ExpandableLabel.h
+++ Classes/ExpandableLabel.h
@@ -9,8 +9,27 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+@class ExpandableLabel;
+
+@protocol ExpandableLabelDelegate <NSObject>
+
+- (void)willExpandLabel:(ExpandableLabel *)label;
+- (void)didExpandLabel:(ExpandableLabel *)label;
+- (void)willCollapseLabel:(ExpandableLabel *)label;
+- (void)didCollapseLabel:(ExpandableLabel *)label;
+
+@end
+
+typedef NS_ENUM(NSUInteger, ExpandableLabelTextReplacementType) {
+    ExpandableLabelTextReplacementTypeCharacter,
+    ExpandableLabelTextReplacementTypeWord,
+};
 
 @interface ExpandableLabel : UILabel
+
+@property (nonatomic, weak) id<ExpandableLabelDelegate> delegate;
+
+
 
 @end
 
